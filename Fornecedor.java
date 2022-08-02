e6708587bbb51b6689fc348f8111a604172e2ffe
JCicero-Code <109885635+JCicero-Code@users.noreply.github.com>
2022-08-01 23:14:58 -0300

    Exercício serialização
    Exercício sobre como serializar em Java

---
+++ Fornecedor.java
@@ -0,0 +1,28 @@
+﻿import java.io.Serializable;
+
+public class Fornecedor implements Serializable {
+      private int id;
+      private String nome;  
+}
+public Fornecedor(int id, String nome){
+      this.setId(id);
+      this.setNome(nome);
+
+}
+
+public int getId() {
+       return id;
+}
+
+public void setId(int id) {
+       this.id = id;
+}
+
+public String getNome() {
+       return nome;
+}
+
+public void setNome(String nome) {
+       this.nome = nome;
+}
+
---
+++ app.java
@@ -0,0 +1,81 @@
+﻿import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputstream;
+import java.io.ObjectOutputStream;
+import java.io.security.cert.TrustAnchor;
+import java.io.util.ArrayList;
+
+
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception{
+     criarArquivoSerializado();
+     lerArquivoSerializado()
+}
+     
+    private static void criarArquivoSerializado(){
+       Fornecedor f1 = new Forcenedor(1, "Peter");
+       FileOutputStream fos = null;
+       ObjectOutputStream oos = null;
+       try{
+           fos = new FileOutputStream("arquivoSerializado.bin");
+           oos = new ObjectOutputStream(fos);
+           oos.writeObject(f1);
+
+      }catch (FileNotFoundException e){
+
+          System.out.printIn("Arquivo não encontrado");
+      }catch (IOException e){
+
+          System.out.printIn("Erro ao criar arquivo");
+      }finally {
+
+        if(oos != null){
+             try{
+                oos.close();
+              }catch(IOException e){
+                 system.out.printIn("Erro ao tentar fechar arquivo");
+                 }
+            }       
+
+       }
+
+  }
+
+//novo metodo
+   private static void lerArquivoSerializado(){
+     Fornecedor f1 = null;
+     FileInputStream fis = null;
+     ObjectInputStream ois = null;
+     try{
+         fis = new FileInputStream("arquivoSerializado.bin");
+         ois = new ObjectInputStream(fis);
+         f1 = (Fornecedor)ois.readObject();
+         System.out.printf("id: %2d\nNome: %s\n",
+          f1.getId(), f1.getNome());
+     }catch (ClassNotFoundException e){
+
+          System.out.printIn("Classe não encontrada");
+      }catch (IOException e){
+
+          System.out.printIn("Erro ao criar arquivo");
+      }finally {
+
+        if(ois != null){
+             try{
+                ois.close();
+              }catch(IOException e){
+                 system.out.printIn("Erro ao tentar fechar arquivo");
+                 }
+            }       
+
+       } 
+}//fecha novo metodo
+
+
+}
